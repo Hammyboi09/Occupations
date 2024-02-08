57979aaac6ea659a92322dc121d1a678dac01719
Hammyboi09 <97976408+Hammyboi09@users.noreply.github.com>
2024-02-08 21:15:27 +0530

    Add files via upload

---
+++ occupations.sql
@@ -0,0 +1,17 @@
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT 
+        Name,
+        Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+    FROM 
+        OCCUPATIONS
+) AS RankedOccupations
+GROUP BY 
+    rn
+ORDER BY 
+    rn;
